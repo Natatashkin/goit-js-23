531bbeefa54881812b41517219eb4fd6a5897244
Natatashkin <cs070283sng@gmail.com>
2021-08-27 11:59:11 +0300

    add files hw 12 countries

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import './styles.css';
+import appendMarkup from './js/appendMarkup';
+
+
+
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,36 @@
+import { error } from "@pnotify/core";
+
+export default class FetchCountries {
+    constructor() {
+        this.searchQuery = '';
+        this.checkInput();
+    }
+
+    fetchCountriesByName() {
+        return fetch(`https://restcountries.eu/rest/v2/name/${this.searchQuery}`)
+            .then(response => (response.ok)
+            ? response.json()
+            : Promise.reject('is not ok: ' + response.status)
+            )
+            .then(countries => {
+                return countries;
+            })
+            .catch( error => console.log(error))
+    }
+
+    checkInput() {
+        if (this.searchQuery = '') {
+            return;
+        }
+    }
+
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
+
